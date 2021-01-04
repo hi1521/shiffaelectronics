5f7aeb70a477562274f8e2092ec06c75babfb403
Hisham Tariq <Hishamtariq.hi@gmail.com>
2021-01-04 16:38:58 +0500

    V4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,63 +1,72 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { MDBFooter, MDBBtn } from "mdbreact";
 
 import { MDBLink } from "mdbreact";
 import axios from "axios";
-import {Redirect} from "react-router";
-
+import { Redirect } from "react-router";
 
 class Copyrights extends Component {
+  state = {
+    logout: false,
+  };
 
-    state = {
-        logout: false
-    }
+  onLogout = () => {
+    axios
+      .post(
+        "http://ec2-3-129-60-50.us-east-2.compute.amazonaws.com/spare_parts/logout.php"
+      )
+      .then((value) => this.setState({ logout: true }));
+  };
 
-    onLogout = () =>{
-        axios.post("http://localhost/spare_parts/logout.php")
-            .then(value => this.setState({logout: true}));
-    }
-
-    render() {
-        if (this.state.logout) return <Redirect to="/login"/>;
-        return (
-            <MDBFooter
-                // className={props.className}
-                style={{...this.props.style, zIndex: 2, backgroundColor: "transparent"}}
-            >
-                <MDBLink to="/sale" target="_blank" style={{
-                    float: "right",
-                }}>
-                    <MDBBtn
-                        size="lg"
-                        gradient="blue"
-                        rounded={true}
-                        style={{
-                            width: "150px",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        {/* <MDBIcon icon="bolt" /> */}
-                        Sale
-                    </MDBBtn>
-                </MDBLink>
-                <MDBBtn
-                    size="lg"
-                    gradient="blue"
-                    rounded={true}
-                    style={{
-                        width: "150px",
-                        float: "right",
-                        marginRight: 50,
-                        marginTop: 15,
-                        fontWeight: "bold",
-                    }}
-                    onClick={() => this.onLogout()}
-                >
-                    Logout
-                </MDBBtn>
-            </MDBFooter>
-        );
-    }
+  render() {
+    if (this.state.logout) return <Redirect to="/login" />;
+    return (
+      <MDBFooter
+        // className={props.className}
+        style={{
+          ...this.props.style,
+          zIndex: 2,
+          backgroundColor: "transparent",
+        }}
+      >
+        <MDBLink
+          to="/sale"
+          target="_blank"
+          style={{
+            float: "right",
+          }}
+        >
+          <MDBBtn
+            size="lg"
+            gradient="blue"
+            rounded={true}
+            style={{
+              width: "150px",
+              fontWeight: "bold",
+            }}
+          >
+            {/* <MDBIcon icon="bolt" /> */}
+            Sale
+          </MDBBtn>
+        </MDBLink>
+        <MDBBtn
+          size="lg"
+          gradient="blue"
+          rounded={true}
+          style={{
+            width: "150px",
+            float: "right",
+            marginRight: 50,
+            marginTop: 15,
+            fontWeight: "bold",
+          }}
+          onClick={() => this.onLogout()}
+        >
+          Logout
+        </MDBBtn>
+      </MDBFooter>
+    );
+  }
 }
 
 export default Copyrights;
